2d837e005319908923a08599338fd34a5fa5e946
Deanna Nishi <nish0018@algonquinlive.com>
2020-03-02 11:40:22 -0500

    e

--- js/index.js
+++ js/index.js
@@ -3,30 +3,38 @@ const content = document.querySelector("main .content")
 let urlHome = "./partials/home.html";
 let urlPortfolio = "./partials/portfolio.html";
 
-//function loadContent(urlValue) {
-	// here is where you want to use your fetch method!
-	//fetch(urlValue)
-	//	.then()
-	//	.then()
-	//	.catch();
-//}
+function loadContent(urlValue) {
+	fetch(urlHome)
+		.then(function(response){
+			return response.text();	
+		})
+	
+		.then(function(homeContent){
+			content.innerHTML = homeContent;
+		})
+		.catch(function(error){
+			console.log(error.message);
+		});
+}
 
-//function handleClick(ev) {
+function handleClick(ev) {
 
-	// preventing the default behaviour of the link tag!
-	//ev.preventDefault();
+	 preventing the default behaviour of the link tag!
+	ev.preventDefault();
 
-	//let clickedLink = ev.target;
+	let clickedLink = ev.target;
 
-	// fetch the value of url
-	//url = clickedLink.href; 
+	 fetch the value of url
+	url = clickedLink.href; 
 
-	//loadContent(url);
-//}
+	loadContent(url);
+}
 
-//for (let link of links) {
-//	//link.addEventListener("click", handleClick);
-//}
+for (let link of links) {
+	//link.addEventListener("click", handleClick);
+}
+
+
 
 function showContent (e) {
 	let clickedLink = e.target;
